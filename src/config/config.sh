663fd9d41839e8e35ec53a22178b01262b6aef74
Gabriel F. Vilar <gabriel@cogumm.net>
2020-12-03 15:02:46 -0300

    #1 - Translations config.sh

--- src/config/config.sh
+++ src/config/config.sh
@@ -2,36 +2,36 @@
 clear
 #---------------------------------------------------------------------------#
 # Data: 06 de Setembro de 2017
-# Última alteração: 15 de Novembro de 2020
-# Nome: Gabriel F. Vilar (CoGUMm)
-# E-mail: gabriel[at]cogumm[dot]net
+# Last update: 15 de Novembro de 2020
+# Name: Gabriel F. Vilar (CoGUMm)
+# Email: gabriel[at]cogumm[dot]net
 # Telegram: http://t.me/CoGUMm
 # Script: config
-# Descrição: Arquivo de configuraçõs da aplicação (s2 DRY KISS s2).
+# Description: Application configuration file (s2 DRY KISS s2).
 #---------------------------------------------------------------------------#
 
 # Script
 SCRIPT="$(basename "$0")"
 
-# Variáveis do projeto.
+# Variables
 TMP_LISTEN=$(mktemp --tmpdir=/tmp shradio.XXXXXXXXXX)
 SITE=https://www.internet-radio.com
 GENRE_LIST=/tmp/genres.list
 
-# Arquivo de configurações e textos das janelas.
+# Importing the file from settings window and texts.
 . src/config/yad.sh
 
-# Cabeçalho de todos os scripts.
+# Header of all scripts.
 head(){
    echo "------------------------------------------------------------------"
-   echo "Idealizador e desenvolvedor: Gabriel F. Vilar (CoGUMm)"
-   echo "E-mail: gabriel[at]cogumm[dot]net"
+   echo "Creator and developer: Gabriel F. Vilar (CoGUMm)"
+   echo "Email: gabriel[at]cogumm[dot]net"
    echo "Telegram: http://t.me/CoGUMm"
    echo "Website: http://cogumm.net"
-   echo "NÃO ME RESPONSABILIZO POR MAUS DANOS À SUA MÁQUINA COM ESTE SCRIPT,"
-   echo "FAVOR UTILIZÁ-LO SE APENAS TIVER CERTEZA DO QUE ESTEJA FAZENDO."
+   echo "I AM NOT RESPONSIBLE FOR BAD DAMAGE TO YOUR MACHINE WITH THIS SCRIPT,"
+   echo "PLEASE USE IT IF YOU ARE JUST SURE WHAT YOU ARE DOING."
    echo "------------------------------------------------------------------"
 }
 
 head
-# Fim
+# End
